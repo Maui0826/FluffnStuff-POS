01321fea1fa4e1331da9015865b7402035f9493f
maumau <134153261+Maui0826@users.noreply.github.com>
2025-08-20 14:51:29 +0800

    7th

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,5 @@
-const API_BASE = import.meta.env.ALLOWED_ORIGINS + '/api/v1';
+// const API_BASE = import.meta.env.ALLOWED_ORIGINS + '/api/v1';
+const API_BASE = 'https://fluffnstuff-pos.onrender.com/api/v1';
 
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.querySelector('#loginForm');
